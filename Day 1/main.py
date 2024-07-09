eb8a221a42cbf8fc65c50ca074d137c33d2ebcd5
Tarane-Konect <tarane.t@konect.ai>
2024-07-09 16:00:17 -0700

    first commit

---
+++ Day 1/main.py
@@ -0,0 +1,6 @@
+print("lets name your pet")
+
+firstname= input("What was the last dish you had: ")
+lastname = input("what's your favourite color: ")
+
+print("your pet name is: "+lastname+ " " + firstname)
