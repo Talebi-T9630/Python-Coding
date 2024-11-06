b5bbfdee27fa847114ff0ec56a000e0ae06c918a
Tarane-Konect <tarane.t@konect.ai>
2024-11-06 12:59:43 -0800

    initial commit

--- Day 2/main.py
+++ Day 2/main.py
@@ -1,8 +1,8 @@
 #Tip Calculator
-total = input("What was the total bill?")
-tip = input("What percentage tip would you like to give? 10, 12, or 15?")
-people = input("How many people are splitting the bill?")
+total = float(input("What was the total bill?"))
+tip = int(input("What percentage tip would you like to give? 10, 12, or 15?"))
+people =int(input("How many people are splitting the bill?"))
 
-final_cost = (float(total)+((float(tip)/100)*float(total)))/float(people)
+final_cost = (total+((tip/100)*total))/people
 
 print(f"Each person pays: {round(final_cost,2)}")
---
+++ ILA Calculator/main.py
@@ -0,0 +1,40 @@
+import time
+
+#ILA Calculator
+hourlyRate= float(input("Please enter the Attorny's Hourly Rate: "))
+IntervalRate= hourlyRate/10
+total= IntervalRate
+print("You can start the timer by pressing t and stop the timer by clicking s.") 
+
+key= input()
+if key == "t" :
+    starttime = time.time()
+    print(f"Timer started at: {time.ctime(starttime)}{starttime}")
+
+key = input()
+if key == "s":
+    endtime =time.time()
+    print(f"Timer ended at: {time.ctime(endtime)}{endtime}")
+    total = (round((endtime-starttime)/360))*IntervalRate
+        
+print(total)
+
+
+
+
+
+    
+
+
+
+
+
+#check timer every 6 minutes, then add interval rate 
+
+
+
+
+
+
+
+
